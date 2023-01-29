e3c422a709e08ffda0290c59a343185179634db8
MABobrov <118075278+MABobrov@users.noreply.github.com>
2023-01-29 22:16:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,52 @@
 #include <stdio.h>
-int a=1;
-int i=1;
-int b=1;
-int t=5;
-void hello() {
-	
-			while(i<10)
-			{
-				printf ("hello №%i \n ",i);
-				i=i+1;
-			}}
-			
 
-void number()
-{
-	
-	while(a<16)
-	{
-		printf("\n %i",a); a=a+1;
-		
-	}}
 
-void chetn(){
-	while(b<16) {
-		
-		if(b % 2 != 0){
-			printf ("\n    %i ",b);
-		}b=b+1;}}
-void zdn_two()
+
+void table_x5(void)
 {
-	while(t<16)
+	printf("Таблица умножения на 5\n");
+	for (int i = 1; i <= 10; i++)
 	{
-		printf("\n    %i ",t);
-		t=t+5;
+		printf("%d * 5 = %d\n", i, i * 5);
 	}
 }
 
-
-
-
-int main(void){ hello(), chetn(),number(),zdn_two();
+void ugaday(void) {
+	int i;
+	printf("Проверь свои экстрасенсорные способности!\n Угадай число: \n");
+	scanf("%d", &i);
 	
+	while (i !=7)
+	{
+		if (i>7)
+			printf("Соберись! число больше загаданного\n");
+		else
+			printf("Открой чакры! число меньше загаданного\n");
+		if (i>10)
+			printf("Между прочим, оно больше 10\n");
+		else
+			if (i<10)
+			printf("Кстати, число меньше 10\n");
+		else
+			printf("Ты попал в десятку, но не угадал\n");
+		if (i%2 == 0)
+			printf("Древние Майа верили, что это число чётное\n");
+		else
+			printf("Шумеры знали, что это числочётное\n");
+		if (i%3 == 0)
+			printf("Наскальные рисунки сведетельствуют что оно делится на 3\n");
+		else
+			printf("Вселенная посылает сигналы о том что оно Не делится 3\n");
+		
+		printf("Не вешай нос! Пробуй ещё: \n");
+		scanf("%d", &i);
+	}
+	printf("Красавчик! Угадал!\n Хорошего тебе дня!\n");
+}
+
+int main(void)
+{
+	table_x5();
+	ugaday();
 	return 0;
 }
